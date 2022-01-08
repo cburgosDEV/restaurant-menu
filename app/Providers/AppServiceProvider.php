4e61a286ab44e7cf574ee09881fff990ba444d78
Cristian <c.burgos@didepti.com>
2022-01-07 19:19:27 -0500

    ENV VERIFICATION

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,8 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        dump(config('app.env'));
-
         if(config('app.env') !== 'local'){
             URL::forceScheme('https');
         }
