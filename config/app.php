f5f8fc0eee788ea13c16487d389ac1acf113b941
Cristian <c.burgos@didepti.com>
2022-01-12 15:05:09 -0500

    without .env

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://menu-cajamarca.herokuapp.com/'),
+    'url' => env('APP_URL', 'https://www.menu-cajamarca.online/'),
 
     'asset_url' => env('ASSET_URL', null),
 
